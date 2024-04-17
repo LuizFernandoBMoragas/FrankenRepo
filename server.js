42e7b19312db2b881d9b9f1310c41aa3cffaa8fa
LuizFernandoBMoragas <luiz.moragas@hotmail.com>
2024-04-17 23:24:59 +0300

    gitignore plus server criation with fastify

---
+++ server.js
@@ -0,0 +1,17 @@
+// import { createServer } from 'node:http';
+
+// const server = createServer((request, response) => {
+//     response.write("Hello")
+    
+//     return response.end();
+// })
+
+// server.listen(3333)
+
+import { fastify } from "fastify";
+
+const server = fastify()
+
+server.listen({
+    port: 3333,
+})
