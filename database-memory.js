d98f3854e85bec197496793d61723853539df4f5
LuizFernandoBMoragas <luiz.moragas@hotmail.com>
2024-04-17 23:50:12 +0300

    Creating database in memory

---
+++ database-memory.js
@@ -0,0 +1,26 @@
+import { randomUUID } from "node:crypto";
+
+
+export class DatabaseMemory {
+    #videos = new Map()
+
+
+    list(){
+        return this.#videos.values();
+    }
+
+
+    create(video){
+        const videosID = randomUUID();
+
+        this.#videos.set(videosID, video);
+    }
+
+    update(id, video){
+        this.#videos.set(id, video);
+    }
+
+    delete(id){
+        this.#videos.delete(id)
+    }
+}
--- server.js
+++ server.js
@@ -1,16 +1,27 @@
-// import { createServer } from 'node:http';
-
-// const server = createServer((request, response) => {
-//     response.write("Hello")
-    
-//     return response.end();
-// })
-
-// server.listen(3333)
-
 import { fastify } from "fastify";
+import { DatabaseMemory } from "./database-memory.js";
 
-const server = fastify()
+const server = fastify();
+
+const database = new DatabaseMemory;
+
+server.post('/videos', () => {  
+    
+})
+
+server.get('/video', () => {
+
+})
+
+server.put('/videos/:id', () => {
+
+})
+
+server.delete('/video/:id', () => {
+
+})
+
+
 
 server.listen({
     port: 3333,
