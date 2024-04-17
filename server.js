c2c1bf839139b0fc7a2022baa4b2a04df94088d2
LuizFernandoBMoragas <luiz.moragas@hotmail.com>
2024-04-17 23:59:01 +0300

    Adding routes

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 import { fastify } from "fastify";
 import { DatabaseMemory } from "./database-memory.js";
+import { title } from "node:process";
 
 const server = fastify();
 
-const database = new DatabaseMemory;
+const database = new DatabaseMemory();
 
-server.post('/videos', () => {  
-    
+server.post('/videos', (request, reply) => {  
+    database.create({
+        title: 'Video 01',
+        description: 'Esse é o primeiro video',
+        duration: 180,
+    })
+
+    console.log(database.list());
+
+    return reply.status(201)
 })
 
 server.get('/video', () => {
