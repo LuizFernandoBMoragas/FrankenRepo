9de56a11a997f13449a540995b9e434e105d7ae2
LuizFernandoBMoragas <luiz.moragas@hotmail.com>
2024-04-19 18:10:05 +0300

    Postgres conection

---
+++ create-table.js
@@ -0,0 +1,11 @@
+import { sql } from "./db.js";
+
+sql`
+    CREATE TABlE videos {
+        title       TEXT,
+        description TEXT,
+        duration    INTEGER
+    };
+`.then(() => {
+    console.log('Tabela criada')
+})
