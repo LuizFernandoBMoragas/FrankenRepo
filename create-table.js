49551e4b6ba5d1040b8808f0d6fab97af39846f8
LuizFernandoBMoragas <luiz.moragas@hotmail.com>
2024-04-21 03:42:28 +0300

    Project Node CRUD with postgres database finished

--- create-table.js
+++ create-table.js
@@ -1,7 +1,10 @@
 import { sql } from "./db.js";
 
+// sql`DROP TABLE IF EXISTS videos`.then(()=>{console.log('Tabela videos foi excluida')})
+
 sql`
     CREATE TABlE videos (
+        id          TEXT PRIMARY KEY,
         title       TEXT,
         description TEXT,
         duration    INTEGER
--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 import { fastify } from "fastify";
-import { DatabaseMemory } from "./database-memory.js";
+// import { DatabaseMemory } from "./database-memory.js";
+import { DatabasePostgres } from "./database-postgres.js";
 
 const server = fastify();
 
-const database = new DatabaseMemory();
+// const database = new DatabaseMemory();
 
-server.post('/videos', (request, reply) => {  
+const database = new DatabasePostgres();
+
+server.post('/videos', async (request, reply) => {  
     const { title, description, duration } = request.body;
 
-    database.create({
+    await database.create({
         title,
         description,
         duration,
@@ -17,20 +20,20 @@ server.post('/videos', (request, reply) => {
     return reply.status(201).send();
 })
 
-server.get('/videos', (request) => {
+server.get('/videos', async (request) => {
     const search = request.query.search;
 
-    const videos = database.list(search);
+    const videos = await database.list(search);
 
     console.log(videos)
     return videos//por padrao o status 200 já significa que deu tudo certo e não inclui-lo. 
 })
 
-server.put('/videos/:id', (request, reply) => {
+server.put('/videos/:id', async (request, reply) => {
     const videoID = request.params.id;
     const { title, description, duration } = request.body;
 
-    database.update(videoID, {
+    await database.update(videoID, {
         title,
         description,
         duration,
@@ -39,10 +42,10 @@ server.put('/videos/:id', (request, reply) => {
     return reply.status(204).send();//204 signfica uma resposta com sucesso porem sem conteudo
 })
 
-server.delete('/videos/:id', (request, reply) => {
+server.delete('/videos/:id', async (request, reply) => {
     const videoId = request.params.id;
 
-    database.delete(videoId)
+    await database.delete(videoId)
 
     return reply.status(204).send();
 })
